9831f5a208e1719715d33ab043a158c2551f90cc
josmolcol <jose.carlos.molina.coloma@gmail.com>
2020-03-25 16:45:16 +0000

    finished

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const cool = require("cool-ascii-faces");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 var app = express();
-
+app.use(bodyParser.json());
 var port = process.env.PORT || 80;
 
 app.use("/",express.static("./public"));
@@ -10,6 +11,31 @@ app.use("/",express.static("./public"));
 app.get("/cool",(request,response)=>{
 	response.send("<html>"+cool()+"</html>");
 });
+
+var contacts = [
+	{
+		name: "peter",
+		phone: 123456
+	},
+	{
+		name: "pablo",
+		phone: 789456
+	}
+];
+
+const BASE_API_URL = "/api/v1";
+
+
+
+app.get(BASE_API_URL+"/contacts",(req,res) =>{
+	res.send(JSON.stringify(contacts,null,2));
+});
+
+app.post(BASE_API_URL+"/contacts",(req,res)=>{
+	contacts.push(req.body);
+	res.sendStatus(201,"CREATED");
+});
+
 
 app.listen(80, () =>{
 	console.log("server ready");
